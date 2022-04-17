8204c9dd4c57ae4b763abd8354c8b9373d123d4c
salles-g <salles.gustavog@gmail.com>
2022-04-17 15:37:05 -0300

    chore(Album): create methods for JSON and DOM scraping

---
+++ src/utils/album.js
@@ -0,0 +1,44 @@
+const { convertTime } = require('./time');
+
+function getAlbumInfoByJSON(json) {
+  return {
+    title: json.name,
+    artist: json.byArtist.name,
+    year: json.datePublished.match(/[0-9]{4}/)[0],
+    cover: json.image,
+    tracks: json.track.itemListElement.map((track) => {
+      return {
+        number: track.position,
+        title: track.item.name,
+        duration: convertTime(track.item.duration),
+      };
+    }),
+  };
+}
+
+function getAlbumInfoByDOM($) {
+  function getTrackInfo($, el) {
+    const number = $(el).find('.track_number').text().slice(0, -1);
+    const title = $(el).find('.track-title').text().trim();
+    const time = $(el).find('.time').text().trim();
+    return {
+      number,
+      title,
+      time,
+    };
+  }
+
+  return {
+    title: $('#name-section .trackTitle').text().trim(),
+    artist: $('#name-section a').text(),
+    year: $('.tralbum-credits').text().trim().slice(-4),
+    cover: $('.popupImage img').attr('src'),
+    tracks: $('.track_list tr')
+      .map((_, el) => {
+        return getTrackInfo($, el, dom);
+      })
+      .get(),
+  };
+}
+
+module.exports = { getAlbumInfoByJSON, getAlbumInfoByDOM };
